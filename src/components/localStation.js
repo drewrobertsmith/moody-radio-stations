094b531b816f97dffd2f8ab0904156e412aece71
drewrobertsmith <drew.robert.smith@gmail.com>
2024-02-20 15:26:23 -0600

    added expo location & permissions

--- src/components/localStation.js
+++ src/components/localStation.js
@@ -1,5 +1,31 @@
+import * as Location from "expo-location";
+
+import { useEffect, useState } from "react";
+
 import { Text } from "react-native";
 
 export default function LocalStation() {
-  return <Text>LocalStation</Text>;
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = "waiting...";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return <Text>{text}</Text>;
 }
