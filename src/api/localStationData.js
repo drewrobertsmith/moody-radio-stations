feaf57b521ece4e47ade66bd6c6d1c0e888f0fe5
drewrobertsmith <drew.robert.smith@gmail.com>
2024-02-20 14:57:30 -0600

    cleaned up local station data

--- src/api/localStationData.js
+++ src/api/localStationData.js
@@ -1,520 +1,378 @@
 export const LOCALSTATIONDATA = [
   {
-    callLetters: "WMBIFM",
-    name: "Radio Moody (Español)",
-    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/WMBIAM.mp3?dist=stationswitcherapp",
-    logo: require("../../assets/stationLogos/Radio-Moody.png"),
-    backgroundColor: "#d19000",
-    accentColor: "#003B5C",
-    textColor: "#FFFFFF",
-  },
-  {
     callLetters: "WCRF",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "103.3 FM - Cleveland",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "W299CY",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "107.7 FM - Charleston",
     lat: 32.82426,
     lng: -80.00267,
   },
   {
     callLetters: "KMBI",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "107.9 FM - Spokane",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "WFCM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
     name: "1200 AM - Nashville",
     lat: 36.208333,
     lng: -86.8725,
   },
   {
     callLetters: "WMBW",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBWFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBWFMAAC.aac",
     name: "88.9 FM - Chattanooga",
     lat: 34.96217,
     lng: -85.37786,
   },
   {
     callLetters: "WMFT",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "88.9 FM - Tuscaloosa/Birmingham",
     lat: 33.33871,
     lng: -87.35886,
   },
   {
     callLetters: "WDLM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
     name: "89.3 FM - East Moline",
     lat: 41.54747,
     lng: -90.47485,
   },
   {
     callLetters: "WRMB",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
     name: "89.3 FM - South Florida",
     lat: 26.51796,
     lng: -80.17034,
   },
   {
     callLetters: "WGNB",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNBFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNBFMAAC.aac",
     name: "89.3 FM - Zeeland",
     lat: 42.83721,
     lng: -85.98831,
   },
   {
     callLetters: "KMBN",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "89.7 FM - Las Cruces",
     lat: 32.2783,
     lng: -106.91191,
   },
   {
     callLetters: "WMBI",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBIFMAAC.aac",
     name: "90.1 FM - Chicago",
     lat: 41.9278,
     lng: -88.00695,
   },
   {
     callLetters: "WJSO",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "90.1 FM - Pikeville",
     lat: 37.46444,
     lng: -82.54528,
   },
   {
     callLetters: "WVML",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "90.5 FM - Millersburg",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WKES",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
     name: "91.1 FM - St. Petersburg",
     lat: 28.07937,
     lng: -82.04075,
   },
   {
     callLetters: "WVMU",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "91.7 FM - Ashtabula",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WFCM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
     name: "91.7 FM - Murfreesboro",
     lat: 35.80019,
     lng: -86.62123,
   },
   {
     callLetters: "WCDC",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "950 AM - Charleston",
     lat: null,
     lng: null,
   },
   {
     callLetters: "WDLM-AM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
     name: "960 AM - East Moline",
     lat: 41.54747,
     lng: -90.47485,
   },
   {
     callLetters: "WGNR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
     name: "97.9 FM - Indianapolis",
     lat: 40.06235,
     lng: -85.70924,
   },
   {
     callLetters: "WFCM",
-    url:
-      "https://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
+    url: "https://playerservices.streamtheworld.com/api/livestream-redirect/WFCMFMAAC.aac",
     name: "98.7 FM - Nashville",
     lat: 35.80019,
     lng: -86.62123,
   },
   {
-    callLetters: "MJST",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/IM_2AAC.aac",
-    name: "Majesty Radio",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "MDRN",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
-    name: "Moody Radio Network",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "PRWR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/IM_1AAC.aac",
-    name: "Praise and Worship",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "PCLM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/IM_4AAC.aac",
-    name: "Proclaim",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "rdmd",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBIAMAAC.aac",
-    name: "Radio Moody (Español)",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "SGR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/IM_5AAC.aac",
-    name: "Southern Gospel Radio",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "URPR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/IM_3AAC.aac",
-    name: "Urban Praise",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: null,
-    url: null,
-    name: "  - ",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "K22EO",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
-    name: "91.9 FM - Hilo",
-    lat: null,
-    lng: null,
-  },
-  {
-    callLetters: "W219AZ",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
-    name: "91.7 FM - Gallipolis",
-    lat: null,
-    lng: null,
-  },
-  {
     callLetters: "W263AH",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
     name: "100.5 FM - Fort Pierce",
     lat: 26.51796,
     lng: -80.17034,
   },
   {
     callLetters: "W218BB",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
     name: "89.5 FM - Okeechobee",
     lat: 26.51796,
     lng: -80.17034,
   },
   {
     callLetters: "KMLW",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "88.3 FM - Moses Lake",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K207DC",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "89.3 FM - Tonasket",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "KMWY",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "91.1 FM - Jackson",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "KSPL",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.9 FM - Kalispell",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K208EF",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "89.5 FM - Missoula",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K234AD",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "94.7 FM - Enterprise",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "WIWC",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
     name: "91.7 FM - Kokomo",
     lat: 40.06235,
     lng: -85.70924,
   },
   {
     callLetters: "WMBL",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
     name: "88.1 FM - Mitchell",
     lat: 40.06235,
     lng: -85.70924,
   },
   {
     callLetters: "WDLM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WDLMFMAAC.aac",
     name: "90.7 FM - Burlington",
     lat: 41.54747,
     lng: -90.47485,
   },
   {
     callLetters: "WMKW",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBWFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WMBWFMAAC.aac",
     name: "89.3 FM - Crossville",
     lat: 34.96217,
     lng: -85.37786,
   },
   {
     callLetters: "WSOR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
     name: "90.9 FM - Naples",
     lat: 26.34165,
     lng: -81.71043,
   },
   {
     callLetters: "WKZM",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
     name: "104.3 FM - Sarasota",
     lat: 28.07937,
     lng: -82.04075,
   },
   {
     callLetters: "W212AC",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WRMBFMAAC.aac",
     name: "90.3 FM - Stuart",
     lat: 26.51796,
     lng: -80.17034,
   },
   {
     callLetters: "K217ER",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "91.3 FM - Pateros",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K264BF",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "100.7 FM - Wenatchee",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K213BN",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.5 FM - Grangeville",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K211AB",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.1 FM - Libby",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K212AO",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.3 FM - Thompson Falls",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K243AA",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "96.5 FM - Union",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "WFOF",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
     name: "90.3 FM - Covington",
     lat: 40.06235,
     lng: -85.70924,
   },
   {
     callLetters: "WMBU",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/MB2AAC.aac",
     name: "89.1 FM - Forest",
     lat: 33.33871,
     lng: -87.35886,
   },
   {
     callLetters: "W210AR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "92.1 FM - Coshocton",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WVMS",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "89.5 FM - Sandusky",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WVME",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "91.9 FM - Meadville",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WVMN",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "90.1 FM - New Castle",
     lat: 41.29679,
     lng: -81.65726,
   },
   {
     callLetters: "WHGN",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WKESFMAAC.aac",
     name: "91.9 FM - Crystal River",
     lat: 28.07937,
     lng: -82.04075,
   },
   {
     callLetters: "K217FR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.7 FM - Bonners Ferry",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K215AD",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.9 FM - Chelan",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K282AA",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "92.9 FM - Kennewick",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K215AB",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "90.9 FM - Grangeville",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "K265AP",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/KMBIFMAAC.aac",
     name: "100.9 FM - Kamiah",
     lat: 47.57051,
     lng: -117.08334,
   },
   {
     callLetters: "WHPL",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WGNRFMAAC.aac",
     name: "89.9 FM - West Lafayette",
     lat: 40.06235,
     lng: -85.70924,
@@ -535,8 +393,7 @@ export const LOCALSTATIONDATA = [
   },
   {
     callLetters: "W210AR",
-    url:
-      "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
+    url: "http://playerservices.streamtheworld.com/api/livestream-redirect/WCRFFMAAC.aac",
     name: "89.9 FM - Cochranton",
     lat: 41.29679,
     lng: -81.65726,
